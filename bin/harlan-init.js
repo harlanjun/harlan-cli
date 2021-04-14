75a42c8a14f4ad362a19ed065c86849f61709fa5
harlan <gongjun@cloudyigou.com>
2021-04-14 19:50:23 +0800

    upd: first

--- bin/harlan-init.js
+++ bin/harlan-init.js
@@ -6,3 +6,45 @@ const path = require('path')
 const tempPath = path.join(__dirname, '../template.json')
 const tempInfo = require(tempPath)
 
+program
+    .usage('<template-name> [project-name]')
+program.parse(process.argv)
+
+//当没有输入参数的时候给个提示
+if(program.args.length<1) return program.help()
+
+// 好比 vue init webpack project-name 的命令一样，第一个参数是 webpack，第二个参数是 project-name
+const templateName = program.args[0]
+const projectName = program.args[1]
+// 小小校验一下参数
+if (!tempInfo[templateName]) {
+    console.log(chalk.red('\n template does not exit! \n'))
+    return
+}
+if(!projectName) {
+    console.log(chalk.red('\n Project should not be empty! \n'))
+    return
+}
+const url = tempInfo[templateName]
+console.log(chalk.white('\n Start generating... \n'))
+// 出现加载图标
+const spinner = ora("Downloading...")
+console.log(url, projectName);
+spinner.start();
+// 执行下载方法并传入参数
+download(
+    url,
+    projectName,
+    err => {
+        if(err) {
+            spinner.fail()
+            console.log(chalk.red(`Generation failed. ${err}`))
+            return
+        }
+        //结束加载图标
+        spinner.succeed();
+        console.log(chalk.green('\n Generation completed!'))
+        console.log('\n To get started')
+        console.log('\n cd ${projectName} \n')
+    }
+)
